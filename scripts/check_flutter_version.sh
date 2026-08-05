4702335dc655ef0557d9c09bfb4bec48bf57017f
Manuel Mauricio Gomez Gallo <mmaurogg@Mac-mini-de-Manuel.local>
2026-08-05 10:19:00 -0500

    ci: ajuste pipeline

--- scripts/check_flutter_version.sh
+++ scripts/check_flutter_version.sh
@@ -1,7 +1,11 @@
-EXPECTED_VERSION="3.38.1"
+EXPECTED_VERSION="3.35.0"
 
 INSTALLED_VERSION=$(flutter --version | head -n 1)
 
+echo "version instalada:"
 echo "$INSTALLED_VERSION"
+
+echo "$version requerida"
+echo "$EXPECTED_VERSION"
 
 echo "$INSTALLED_VERSION" | grep "$EXPECTED_VERSION"
