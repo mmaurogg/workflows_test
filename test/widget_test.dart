387cbe6d0f06e433a262a241a91e3c945325ed8f
Manuel Mauricio Gomez Gallo <mmaurogg@Mac-mini-de-Manuel.local>
2026-08-11 11:27:37 -0500

    feat: añadida la funcion decrementar y test de integracion

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -27,4 +27,21 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+
+  testWidgets('Counter decrements smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('-1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.minimize));
+    await tester.pump();
+
+    // Verify that our counter has decremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('-1'), findsOneWidget);
+  });
 }
